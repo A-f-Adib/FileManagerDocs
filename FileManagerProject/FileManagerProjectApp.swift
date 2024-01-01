68cbf3c86204e66505c354ee64b78945170d623c
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-01-01 07:00:17 +0600

    Initial Commit

---
+++ FileManagerProject/FileManagerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerProjectApp.swift
+//  FileManagerProject
+//
+//  Created by A.f. Adib on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
